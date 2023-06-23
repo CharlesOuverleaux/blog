9f981d21c2b0c0eec3ab69353a320bcb46601e04
CharlesOuverleaux <charlesantoine.ouverleaux@gmail.com>
2023-06-23 12:39:56 +0100

    Add helper function JSDOC

--- helpers/getCtaText.ts
+++ helpers/getCtaText.ts
@@ -1,5 +1,12 @@
 import blogData from "../data/blogData.json";
 
+/**
+ * Get the Call-to-Action (CTA) text based on the sign-up status and variation.
+ *
+ * @param {boolean} isSignedUp - The sign-up status.
+ * @param {string} variation - The variation identifier.
+ * @returns {string} The CTA text.
+ */
 export const getCtaText = (isSignedUp, variation) => {
   const { testContent } = blogData;
   const ctaText = isSignedUp
